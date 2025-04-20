ae485a1802c778c6b0775992f3e9bd3b21f4788b
sas26031962 <sasx26031962@gmail.com>
2025-04-20 11:20:48 +0300

    Разборка строки тип 1 и тип 2 на дату и время

--- main.cpp
+++ main.cpp
@@ -25,15 +25,35 @@ int main(int argc, char *argv[])
     bool match1 = re1.match(input1.toLower()).hasMatch();
     bool match2 = re2.match(input2.toLower()).hasMatch();
 
-    if (match1) {
-        qDebug() << "Строка " << input1 << " соответствует регулярному выражению:" << pattern1;
-    } else {
+    int Year, Month, Day, Hour, Min, Sec;
+
+    if (match1)
+    {
+        Year = input1.mid(0, 4).toInt();
+        Month = input1.mid(4, 2).toInt();
+        Day = input1.mid(6, 2).toInt();
+        Hour = input1.mid(9, 2).toInt();
+        Min = input1.mid(11, 2).toInt();
+        Sec = input1.mid(13, 2).toInt();
+        qDebug() << "Строка " << input1 << " is Ok for:" << pattern1 << ": Year=" << Year << " Month=" << Month << " Day=" << Day << " Hour=" << Hour << " Min=" << Min << " Sec=" << Sec;
+    }
+    else
+    {
         qDebug() << "Строка " << input1 << " не соответствует регулярному выражению:" << pattern1;
     }
 
-    if (match2) {
-        qDebug() << "Строка " << input2 << " соответствует регулярному выражению:" << pattern2;
-    } else {
+    if (match2)
+    {
+        Year = input2.mid(0, 4).toInt();
+        Month = input2.mid(5, 2).toInt();
+        Day = input2.mid(8, 2).toInt();
+        Hour = input2.mid(11, 2).toInt();
+        Min = input2.mid(14, 2).toInt();
+        Sec = input2.mid(17, 2).toInt();
+        qDebug() << "Строка " << input1 << " is Ok for:" << pattern2 << ": Year=" << Year << " Month=" << Month << " Day=" << Day << " Hour=" << Hour << " Min=" << Min << " Sec=" << Sec;
+    }
+    else
+    {
         qDebug() << "Строка " << input2 << " не соответствует регулярному выражению:" << pattern2;
     }
 
